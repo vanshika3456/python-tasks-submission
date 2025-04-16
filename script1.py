e8798f5f1965ea333b4aa0ad44be7b48d0dfeacc
VANSHIKA JAITIK <jaitikvanshika@gmail.com>
2025-04-16 21:33:46 +0530

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,10 @@
+#task 1
+# perform basic mathmatical
+# Takes two numbers as input from the user.
+a= int(input("ENTER THE  FIRST NUMBER:"))
+b =int(input("ENTER THE  SECOND NUMBER:"))
+#Performs the basic mathematical operations on these two numbers:
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
---
+++ script2.py
@@ -0,0 +1,9 @@
+#task 2
+#Create a Personalized Greeting
+# Takes a user's first name and last name as input
+first =input("Enter your first name:")
+last=input("Enter your last name:")
+#Concatenates the first name and last name into a full name.
+fullname= first+ last
+#Prints a personalized greeting message using the full name.
+print("Hello",fullname,"!welcome to the python program")
